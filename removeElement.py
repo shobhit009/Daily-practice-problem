4340aaa8a2ed82240fe3b68270e6c617e8bd3758
shobhit009 <shobhit.kumar2410@gmail.com>
2022-05-06 15:27:46 +0530

    Changing User name

--- removeElement.py
+++ removeElement.py
@@ -33,6 +33,7 @@
 # Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
+#checking change of user
 from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
